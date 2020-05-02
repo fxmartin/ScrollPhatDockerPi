dbcbec7e8f16f0fb07bfe21cef312427027ba216
FX Martin <mail@fxmartin.me>
2020-05-02 17:40:37 +0200

    Revised version of the Python program

--- monitor.py
+++ monitor.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-import math
-import os
-import sys
-import time
-import psutil
+import argparse
 import scrollphat
+import time
+import sys
 from subprocess import PIPE, Popen
+import psutil
 import docker
 
 def get_cpu_temperature():
@@ -21,39 +20,67 @@ def get_cpu_temperature():
 
     return temp
 
-scrollphat.set_brightness(100)
-scrollphat.set_rotate(1)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v','--verbose',action='store_true',help='increase output verbosity')
+    parser.add_argument('-b','--brightness',help='set the brightness (default is 25)',type=int,default=25)
+    parser.add_argument('-r','--rotate',action='store_true',help='Rotate the display on the ScrollPhate',default=False)
+    parser.add_argument('-i','--interval',help='set the refresh interval (default is 0.05)',type=int,default=0.05)
+    parser.add_argument('-t','--temperature',help='Define on which line the temperature graph is displayed (default is 0)',type=int,default=0)
+    parser.add_argument('-c','--cpu',help='Define on which line the cpu load graph is displayed (default is 1)',type=int,default=1)
+    parser.add_argument('-d','--docker',help='Define on which line the number of docker containers is displayed (default is 4)',type=int,default=4)
+    args = parser.parse_args()
+    temp = get_cpu_temperature()
+    client = docker.from_env()
+    nb_containers = len(client.containers.list())
+    if args.verbose:
+        print('monitor.py - program wrote by FX')
+        print('Last modification: 02-may-2020')
+        print('Verbosity mode turned on')
+        print('Press Ctrl+C to exit!')
+        print('Brightness: '+str(args.brightness))
+        print('CPU temperature: '+str(temp))
+        print('Rotate: '+str(args.rotate))
+        print('Interval: '+str(args.interval))
+        print('Number of docker containers running: '+str(nb_containers))
+    scrollphat.set_rotate(args.rotate)
+    scrollphat.set_brightness(args.brightness)
 
-print('CPU temperature: ' + str(get_cpu_temperature()))
-
-load = psutil.cpu_percent(percpu=1)
-
-for c in range(4):
-	print("CPU " + str(c) + " load: " + str(load[c]))
-
-print('CPU Load: ' + str(load))
-
-client = docker.from_env()
-for container in client.containers.list():
-  print(container.id)
-
-print("Number of docker machines running: " + str(len(client.containers.list())))
-
-while(True):
-    try:
-        for c in range (4):
-            for x in range (int(psutil.cpu_percent(percpu=1)[c]/10)):
-                scrollphat.set_pixel(x,c,1)
-
-        for x in range (int(get_cpu_temperature()/10)):
-            scrollphat.set_pixel(x,4,1)
-
-        for x in range (int(len(client.containers.list()))):
-            scrollphat.set_pixel(10,4-x,1)
-
-        scrollphat.update()
-        scrollphat.clear()
-
-    except KeyboardInterrupt:
-        scrollphat.clear()
-        sys.exit(-1)
+    prev_temp = 0
+    prev_load = 0
+    prev_nb_containers = 0
+    while True:
+        try:
+            # Display the temperature
+            temp = get_cpu_temperature()
+            if (temp>prev_temp):
+                for x in range(int(temp/80*11)):
+                    scrollphat.set_pixel(x,args.temperature,1)
+            elif (temp<prev_temp):
+                for x in range(int(temp/80*11),int(prev_temp/80*11)):
+                    scrollphat.set_pixel(x,args.temperature,0)
+            prev_temp = temp
+            # Display the cpu load
+            load = psutil.cpu_percent()
+            if (load>prev_load):
+                for x in range(int(load/100*11)):
+                    scrollphat.set_pixel(x,args.cpu,1)
+            elif (load<prev_load):
+                for x in range(int(load/100*11),int(prev_load/100*11)):
+                    scrollphat.set_pixel(x,args.cpu,0)
+            prev_load = load
+            # Display the number of containers running
+            nb_containers = len(client.containers.list())
+            if (nb_containers>prev_nb_containers):
+                for x in range(int(nb_containers)):
+                    scrollphat.set_pixel(x,args.docker,1)
+            elif (nb_containers<prev_nb_containers):
+                for x in range(int(nb_containers),int(prev_nb_containers)):
+                    scrollphat.set_pixel(x,args.docker,0)
+            prev_nb_containers = nb_containers
+            scrollphat.update()
+            time.sleep(args.interval)
+        except KeyboardInterrupt:
+            # shut off pixels and exit
+            scrollphat.clear()
+            sys.exit(-1)
